ff795dd29966cd486edd23a3307c7434398450b0
hs414171 <hs414171@gmail.com>
2021-12-30 15:40:03 +0530

    .env solved?

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
-require('dotenv').config()
+const path = require('path')
+require('dotenv').config({path: __dirname +'/.env'});
 const express = require('express')
 const mongoose = require('mongoose')
 const user_routes = require('./routes/user_routes')
+
 // const prod_routes = require('./routes/product_routes')
 
 const cors = require('cors')
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 const app = express()
+console.log(__dirname)
+
+
 // var options = {
 //     "origin": "*",
 //     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
 //     "preflightContinue": false,
 //     "optionsSuccessStatus": 204
 //   }
-  app.use(cors());
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
+app.use(cors());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/',(req,res)=>{
     res.send("Hello World")
@@ -35,7 +40,7 @@ db.on('error', (error)=>{console.log(error)});
 db.once('open', ()=>{console.log('Connected to Database')});
 app.use(express.json())
 app.use('/api/user',user_routes)
-// app.use('/api/product',prod_routes)
+
 
 
 
